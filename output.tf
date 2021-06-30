5099f362cd516a9ed9b96286513334d5a98ca6b7
Anirudh-77 <shingalaanirudh@gmail.com>
2021-06-30 18:15:10 +0530

    update output

--- output.tf
+++ output.tf
@@ -1,10 +1,3 @@
-#--Output instance id
-
-# output "test-instance" {
-#   value = data.aws_ami.latest-linux-ami.id
-# }
-
-
 output "public-ip" {
   value = module.myapp-server.instance.public_ip
 }
